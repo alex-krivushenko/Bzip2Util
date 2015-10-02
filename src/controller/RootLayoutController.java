ce6c4dd31f0e18309397fc84ccfff55b43fe80d3
Alexander Krivushenko <alex.krivushenko@gmail.com>
2015-10-02 17:16:05 +0300

    Small changes

--- src/controller/RootLayoutController.java
+++ src/controller/RootLayoutController.java
@@ -3,5 +3,5 @@ package controller;
 /**
  * Created by Александр on 02.10.2015.
  */
-public class MenuController {
+public class RootLayoutController {
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,14 +6,32 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+    private Stage primaryStage;
+    private Parent root;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../model/sample.fxml"));
-        primaryStage.setTitle("ZipUtil");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        this.primaryStage = primaryStage;
+        primaryStage.setTitle("Super Archiver");
+
+        initRootLayout();
+    }
+
+    public void initRootLayout() {
+        try {
+            root = FXMLLoader.load(getClass().getResource("../view/RootLayout.fxml"));
+            primaryStage.setScene(new Scene(root, 300, 275));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showFilesOverwies() {
+
     }
 
     public static void main(String[] args) {
